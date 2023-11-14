d968087c962a757fa737b993707c1d22fbfce52d
M4RZ3R0 <m4rz3r0.development@gmail.com>
2023-11-14 21:15:42 +0100

    OB-46 #time 1h #DONE #comment Diseño de la interfaz de las pantallas de registro e inicio de sesión hecho

---
+++ app/src/main/java/com/aseegpsproject/openbook/view/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.aseegpsproject.openbook.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.aseegpsproject.openbook.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
