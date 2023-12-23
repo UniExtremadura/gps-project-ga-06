c55cc2f0829afe6fd43e944f3cbdea7dd7b45c21
M4RZ3R0 <m4rz3r0.development@gmail.com>
2023-12-23 13:12:18 +0100

    Tests unitarios User

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,6 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
     implementation("androidx.preference:preference-ktx:1.2.1")
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.test:runner:1.5.2")
